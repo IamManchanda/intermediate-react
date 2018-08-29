3144e46f0f53039fdd147ebb56d4b2415b395f73
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-29 16:38:14 +0530

    Refactor: Make main app component a functional component!

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,22 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { Router, Link } from '@reach/router';
 import Results from './pages/results';
 import Details from './pages/details';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <Link to="/">
-            Adopt Me!
-          </Link>
-        </header>
-        <Router>
-          <Results path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">
+        Adopt Me!
+      </Link>
+    </header>
+    <Router>
+      <Results path="/" />
+      <Details path="/details/:id" />
+    </Router>
+  </div>
+);
 
 const root = document.getElementById('root');
 render(<App />, root);
