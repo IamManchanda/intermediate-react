07517b25b95be985d6821d8d0737898e08771afc
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-09-02 15:00:40 +0530

    Implement to Node Stream!

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream } from 'react-dom/server';
 import { ServerLocation } from '@reach/router';
 import fs from 'fs';
 import App from './src/js/app';
@@ -14,14 +14,20 @@ const server = express();
 
 server.use('/dist', express.static('dist'));
 server.use((req, res) => {
+  res.write(parts[0]);
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
-
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup);
+  stream.pipe(res, {
+    end: false,
+  });
+  stream.on('end', () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 server.listen(PORT);
